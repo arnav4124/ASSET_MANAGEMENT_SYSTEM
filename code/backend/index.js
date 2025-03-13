1e52e8496d226e788c37ae4ef2a3fa3d187d9208
MrudaniPimpalkhare <you@example.com>
2025-03-14 00:28:12 +0530

    fixed schema bugs

--- code/backend/index.js
+++ code/backend/index.js
@@ -8,6 +8,8 @@ const multer = require("multer");
 const path = require("path");
 const axios = require("axios");
 const xml2js = require("xml2js");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // require("dotenv").config();
 require("dotenv").config({ path: ".env" });
@@ -15,6 +17,14 @@ const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
+
+// schemas
+const User = require("./models/user");
+const Location = require("./models/location"); // Location model is exported as Itemmodel, not Location
+const Asset = require("./models/asset");
+const Programme = require("./models/programme");
+const Invoice = require("./models/invoice");
+const Project = require("./models/project");    
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,12 +36,8 @@ mongoose.connect(MONGO_URI)
         console.error("MongoDB connection error:", err);
         process.exit(1);
     });
-const User = require("./models/user");
-const Location = require("./models/location"); // Location model is exported as Itemmodel, not Location
-const Asset = require("./models/asset");
-const Programme = require("./models/programme");
-const Invoice = require("./models/invoice");
-const Project = require("./models/project");    
+
+
 
 
 
