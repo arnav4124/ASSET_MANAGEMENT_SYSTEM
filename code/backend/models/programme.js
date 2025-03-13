998d78847f28dab492b3f38cad16e29548a30318
arnav4124 <arnav.deshmukh@students.iiit.ac.in>
2025-03-13 22:41:21 +0530

    added programme model

---
+++ code/backend/models/programme.js
@@ -0,0 +1,20 @@
+const {Schema,model} = require('mongoose');
+
+const programmeSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    programme_type:{
+        type:String,
+        required:true
+    },
+    programmes_description:{
+        type:String,
+        required:true
+    },
+})
+
+const Programme = model('Programme',programmeSchema);
+
+module.exports = Programme;
