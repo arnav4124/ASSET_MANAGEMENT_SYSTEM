58defb6371e12141b837f9e7815c68f35375223e
arnav4124 <arnav.deshmukh@students.iiit.ac.in>
2025-03-13 22:33:20 +0530

    estabilshed mongo DB connecion eureka

--- code/backend/index.js
+++ code/backend/index.js
@@ -20,12 +20,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log(err));
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    });
+const User = require("./models/user");
+const Location = require("./models/location"); // Location model is exported as Itemmodel, not Location
 
 
 
