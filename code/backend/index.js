6c4e5ebe64da9d7426651fa3e55ce8fac4675c5c
arnav4124 <arnav.deshmukh@students.iiit.ac.in>
2025-03-13 21:18:48 +0530

    some more backend changes

---
+++ code/backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const axios = require("axios");
+const xml2js = require("xml2js");
+
+// require("dotenv").config();
+require("dotenv").config({ path: ".env" });
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err));
+
+
+
