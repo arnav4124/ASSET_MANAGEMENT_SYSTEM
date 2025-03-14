6f7657a2aea284958f1e76b875f349a34d856243
arnav4124 <arnav.deshmukh@students.iiit.ac.in>
2025-03-14 23:59:17 +0530

    merged conflict

--- code/backend/middleware/auth.js
+++ code/backend/middleware/auth.js
@@ -1,28 +1,28 @@
 const jwt = require("jsonwebtoken");    
 
 function authMiddleware(req, res, next){
-<<<<<<< HEAD
+
     // const {token}=req.headers;
     // if(!token){
     //     return res.json({success:false,message:'Unauthorized access'});
     // }
-=======
+
     console.log("BURHUHU")
     const {token}=req.headers;
     if(!token){
         console.log("TOKEN NHI H    ")
         return res.json({success:false,message:'Unauthorized access'});
     }
->>>>>>> 71b4c1b6f8ed5f9391df31bb813fd21077737e17
 
-    // try {
-    //     const tokendecode=jwt.verify(token,process.env.JWT_SECRET);
-    //     req.body.userId=tokendecode.id;
-    //     next();
-    // } catch (error) {
-    //     console.log(error);
-    //     return res.json({success:false,message:'Unauthorized access'});
-    // }
+
+    try {
+        const tokendecode=jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userId=tokendecode.id;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.json({success:false,message:'Unauthorized access'});
+    }
     next();
 }
 
