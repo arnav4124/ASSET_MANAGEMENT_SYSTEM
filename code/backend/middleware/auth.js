665e32a0fa1211d6d789146e498cc3c975516af9
Nidhish Jain <149590210+Nidhishj@users.noreply.github.com>
2025-03-15 14:36:51 +0530

    error_handled? auth.js

--- code/backend/middleware/auth.js
+++ code/backend/middleware/auth.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 function authMiddleware(req, res, next){
 
     // const {token}=req.headers;
-    if(!token){
-        return res.json({success:false,message:'Unauthorized access'});
-    }
+    // if(!token){
+    //     return res.json({success:false,message:'Unauthorized access'});
+    // }
 
     console.log("BURHUHU")
     const {token}=req.headers;
